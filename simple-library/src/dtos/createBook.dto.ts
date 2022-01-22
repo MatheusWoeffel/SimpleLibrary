8153894b44a261d7a382f079c7a0d51ee2dd8137
MatheusWoeffel <mwcamargo@inf.ufrgs.br>
2022-01-22 15:25:40 -0300

    Added more classValidations on createBookDto

--- simple-library/src/dtos/createBook.dto.ts
+++ simple-library/src/dtos/createBook.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty } from "class-validator";
+import { IsArray, IsDefined, IsNotEmpty, IsOptional } from "class-validator";
 
 /* eslint-disable prettier/prettier */
 export class CreateBookDto {
   @IsNotEmpty()
   title: string;
+  @IsOptional()
   synopsis: string | null;
+  
+  @IsArray()
+  @IsDefined()
   genres: string[];
 }
